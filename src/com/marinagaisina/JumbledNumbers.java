12588fab84ffd0417f39a20db34751ebdb54e21e
Marina Gaisina <farigatovna@hotmail.com>
2022-01-03 23:50:50 -0500

    Trying to work it out with regular expression. No luck, picking another solution

--- src/com/marinagaisina/JumbledNumbers.java
+++ src/com/marinagaisina/JumbledNumbers.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class JumbledNumbers {
     public static void main(String[] args) throws IOException {
@@ -17,7 +15,37 @@ public class JumbledNumbers {
         while ((line = in.readLine()) != null) {
             str += line;
         }
-        String[] charArr = str.split("");
+        String[] letterArr = str.split("");
+        Set<String> result = new HashSet<>();
+
+        if (str.contains("o") && (str.contains("n") && (str.contains("e")))) {
+            str = str.replace("o", "").replace("n", "").replace("e", "");
+            result.add("one");
+        }
+        if (str.contains("t") && (str.contains("w") && (str.contains("o")))) {
+            str = str.replace("t", "").replace("w", "").replace("o", "");
+            result.add("two");
+        }
+        if (str.contains("t") && (str.contains("h")) && (str.contains("r")) && (str.contains("e"))) {
+            str = str.replace("t", "").replace("h", "").replace("r", "");
+            result.add("three");
+        }
+        if (str.contains("f") && (str.contains("o")) && (str.contains("u")) && (str.contains("r"))) {
+            str = str.replace("o", "").replace("n", "").replace("e", "");
+            result.add("four");
+        }
+        if (str.contains("f") && (str.contains("i")) && (str.contains("v")) && (str.contains("e"))) {
+            str = str.replace("o", "").replace("n", "").replace("e", "");
+            result.add("five");
+        }
+        if (str.contains("s") && (str.contains("i")) && (str.contains("x"))) {
+            str = str.replace("o", "").replace("n", "").replace("e", "");
+            result.add("six");
+        }
+        if (str.contains("s") && (str.contains("e")) && (str.contains("v")) && (str.contains("e")) && (str.contains("n"))) {
+            str = str.replace("o", "").replace("n", "").replace("e", "");
+            result.add("three");
+        }
         Map<String, Integer> map = new HashMap<>();
         map.put("one", 1);
         map.put("two", 2);
@@ -30,11 +58,6 @@ public class JumbledNumbers {
         map.put("nine", 9);
         map.put("zero", 0);
 
-        String number;
-        for (String letter : charArr) {
-            if (letter.equals("o")) {
 
-            }
-        }
     }
 }
